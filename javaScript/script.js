487d202433a9d24d729294726184fa3bf4ca1078
Breno Tardelli <brenotardelli@brenos-macbook-pro.home>
2025-02-27 12:06:14 +0100

    some css updates and scroll smooth added

--- javaScript/script.js
+++ javaScript/script.js
@@ -8,5 +8,25 @@ function menuMobile() {
     mobileMenu.classList.toggle("active", !isActive);
   });
 }
-
 menuMobile();
+
+function scrollSmooth() {
+  const links = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  links.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+scrollSmooth();
